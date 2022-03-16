2bf56d5d9c833858997ea7eaef8eabc5f2bc6d20
nikini888 <luutuyen37k8@gmail.com>
2022-03-16 23:07:19 +0700

    Fixed bug and key code
    - And key code, css key down
    - Refactor code

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const numbs = document.querySelectorAll('.btn--number')
+const btnNumbs = document.querySelectorAll('.btn--number')
 const btnGroupOperators = document.querySelector('.btn--operators')
-const btnEqual = document.querySelector('.equal')
+
 const showInsertNumb = document.querySelector('.show-insert')
 const showCurrentNumb = document.querySelector('.show-current')
 const showResult = document.querySelector('.show-result')
 const operatorIcon = document.querySelector('#operatorIcon')
 
 const btnNegativeNumb = document.querySelector('.isnegative')
-const btnDelete = document.querySelector('.del')
+
+const btnBackspace = document.querySelector('.del')
 const btnPoint = document.querySelector('.point')
+const btnEqual = document.querySelector('.equal')
+const btnDelete = document.querySelector('.reset')
+
 const empty = 'empty'
 let isEndCalculator = false;
-let isFloatNumb = false;
-// document.addEventListener('keyup', (e) => {
-//     console.log(e.key)
-//     console.dir(e)
-// })
+
+
 function getLocalFormatDecimal() {
     const numb = 1.1;
     return numb.toLocaleString().slice(1, 2)
@@ -27,7 +28,6 @@ const data = {
     operate: '',
 }
 
-console.log(data.numbCurrent)
 const emptyData = {
     numbCurrent: 0,
     result: 0,
@@ -68,115 +68,255 @@ function multiply(a, b) {
     return a * b
 }
 function divide(a, b) {
+    if (b === 0) {
+        alert('Cann\'t divide 0')
+        resetData()
+        return 0
+    }
     return Number.isInteger(a / b) ? a / b : Number.parseFloat((a / b).toFixed(3))
 }
 
 function countResult(a, b, operator) {
+    let result;
     switch (operator) {
         case 'divide':
-            data.result = divide(a, b)
+            result = divide(a, b)
             break;
         case 'multiply':
-            data.result = multiply(a, b)
+            result = multiply(a, b)
             break;
         case 'subtract':
-            data.result = subtract(a, b)
+            result = subtract(a, b)
             break;
         case 'plus':
-            data.result = plus(a, b)
+            result = plus(a, b)
             break;
         default:
             console.log('Something went wrong')
     }
+    data.result = result
+}
+
+function styleNumber(str) {
+    return str.toLocaleString(undefined, {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 3
+    })
+}
+
+function checkFloatDigit(str) {
+    if (str.split(localDecimal)[1].length === 3) return true
 }
 
 
+function isIncludeDecimal(str) {
+    return str.includes(localDecimal)
+}
+
 function changeTextContent(el, string) {
-    if (data.operate) {
-        const icon = operators.find(e => e.name === data.operate).icon
-        operatorIcon.classList.add(...icon)
-    }
     if (string === 'empty') {
         el.innerText = ''
     } else {
         if (typeof string === 'number') {
-            el.innerText = Intl.NumberFormat().format(string)
+            el.innerText = styleNumber(string)
         } else {
             el.innerText = el.innerText + string
         }
     }
 }
+
+function addIcon() {
+    if (data.operate) {
+        const icon = operators.find(e => e.name === data.operate).icon
+        operatorIcon.classList.add(...icon)
+    }
+}
+
+function removeIcon() {
+    operatorIcon.removeAttribute('class')
+}
+
+function resetContent() {
+    removeIcon()
+    changeTextContent(showCurrentNumb, empty)
+    changeTextContent(showInsertNumb, empty)
+}
+
 function resetData() {
     for (let key in emptyData) {
         data[key] = emptyData[key]
     }
-    operatorIcon.removeAttribute('class')
-    changeTextContent(showCurrentNumb, empty)
-    changeTextContent(showInsertNumb, empty)
+    resetContent()
+    isPoint = false;
+    isEndCalculator = false;
+    changeTextContent(showResult, data.result)
+
+}
+function actionNumb(key) {
+    if (showResult.innerText === '0' && (key === '00' || key === '0')) {
+        return
+    }
+
+    if (isEndCalculator) {
+        resetContent()
+        data.result = parseInt(key)
+        isEndCalculator = false;
+    } else if (isIncludeDecimal(showResult.innerText)) {
+        if (checkFloatDigit(showResult.innerText)) {
+            return
+        }
+        data.result = parseFloat(showResult.innerText.split(localDecimal).join('.') + key)
+        if ((key === '00' || key === '0')) {
+            showResult.innerText = showResult.innerText + key
+            return
+        }
+
+    } else {
+        data.result = parseFloat(data.result + key)
+    }
     changeTextContent(showResult, data.result)
 }
-document.querySelector('.reset').addEventListener('click', resetData)
-numbs.forEach(num => {
-    num.addEventListener('click', (e) => {
-        if (showResult.innerText === 0 && e.target.dataset.num === '00') return
-        if (isEndCalculator) {
-            resetData()
-            isEndCalculator = false;
-        }
-        if (isFloatNumb) {
-            if (e.target.dataset.num === '0' || e.target.dataset.num === '00') {
-                changeTextContent(showResult, e.target.dataset.num)
-                return
-            } else {
-                isFloatNumb = false
-                if (data.result === 0) {
-                    data.result = parseFloat(showResult.innerText.split(localDecimal).join('.') + e.target.dataset.num)
-                } else {
-                    data.result = data.result + parseFloat(`0.${e.target.dataset.num}`)
-                }
-            }
-        } else {
-            data.result = parseFloat(data.result + e.target.dataset.num)
-        }
-        changeTextContent(showResult, data.result)
-    }
-    )
-})
-
-btnGroupOperators.addEventListener('click', (e) => {
+function actionOperator(operate) {
+    removeIcon()
     if (isEndCalculator) {
-        changeTextContent(showInsertNumb, '')
+        resetContent()
         isEndCalculator = false;
     }
-    data.operate = e.target.closest('.btn--operator').dataset.operator
-    data.numbCurrent = data.result;
-    data.result = 0;
-    console.log('Tinh', data)
+    data.operate = operate
+    data.numbCurrent = data.result
+    data.result = 0
     changeTextContent(showCurrentNumb, data.numbCurrent)
-})
-btnEqual.addEventListener('click', () => {
+    changeTextContent(showResult, empty)
+    addIcon()
+}
+function actionEqual() {
     isEndCalculator = true;
     changeTextContent(showInsertNumb, data.result)
     countResult(data.numbCurrent, data.result, data.operate);
     changeTextContent(showResult, data.result)
     data.numbCurrent = data.result
-})
-btnNegativeNumb.addEventListener('click', () => {
+}
+function actionPoint() {
+    if (isIncludeDecimal(showResult.innerText)) {
+        return
+    }
+    if (isEndCalculator) {
+        resetContent()
+        isEndCalculator = false;
+    }
+    changeTextContent(showResult, localDecimal)
+}
+function actionBackspace() {
+    if (isEndCalculator) {
+        resetData()
+        isEndCalculator = false;
+    } else {
+        if (isIncludeDecimal(showResult.innerText)) {
+            if (data.result === 0) {
+                showResult.innerText = showResult.innerText.replace(/.$/, '')
+                return
+            }
+            if (data.result !== 0) {
+                data.result = parseFloat(data.result.toString().replace(/.$/, ''))
+                if (data.result === 0) {
+                    showResult.innerText = showResult.innerText.replace(/.$/, '')
+                    return
+                }
+            }
+        } else if (showResult.innerText.length < 2) {
+            data.result = 0
+        } else {
+            data.result = parseFloat(data.result.toString()
+                .replace(/.$/, ''))
+        }
+    }
+    changeTextContent(showResult, data.result)
+}
+function actionNegative() {
+    if (data.result === 0) {
+        if (showResult.innerText === '0') return
+        showResult.innerText = `- ${showResult.innerText}`
+        return
+    }
+    if (isEndCalculator) {
+        resetContent()
+        isEndCalculator = false;
+    }
     data.result = data.result * -1;
     changeTextContent(showResult, data.result)
-})
-btnDelete.addEventListener('click', () => {
-    if (data.set === 0) return
-    data.result = parseFloat(data.result.toString().replace(/.$/, ''));
-    changeTextContent(showResult, data.result)
-})
-btnPoint.addEventListener('click', () => {
-    isFloatNumb = true;
-    changeTextContent(showResult, localDecimal)
-})
-function deleteTheLastOneString(el) {
-    el.innerText = el.innerText.replace(/.$/, '')
 }
-function addTheLastOneString(el, numb) {
-    el.innerText = el.innerText + numb
+btnDelete.addEventListener('click', resetData)
+btnNumbs.forEach(num => {
+    num.addEventListener('click', (e) => {
+        actionNumb(e.target.dataset.num)
+    })
+})
+
+btnGroupOperators.addEventListener('click', (e) => {
+    actionOperator(e.target.closest('.btn--operator').dataset.operator)
+})
+btnEqual.addEventListener('click', actionEqual)
+btnNegativeNumb.addEventListener('click', actionNegative)
+btnBackspace.addEventListener('click', actionBackspace)
+
+btnPoint.addEventListener('click', actionPoint)
+
+
+//Use keyup
+
+const keyNumbList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const pointNumb = { name: 'point', key: '.' }
+const keyEqual = [
+    { name: 'equal', key: '=' },
+    { name: 'equal', key: 'Enter' },
+]
+const operateKey = [
+    { name: 'divide', key: '/' },
+    { name: 'multiply', key: '*' },
+    { name: 'subtract', key: '-' },
+    { name: 'plus', key: '+' },
+]
+const backspace = { name: 'del', key: 'Backspace' }
+const resetAc = { name: 'reset', key: 'Delete' }
+
+function btnActive(btn) {
+    btn.classList.add('active')
+    btn.addEventListener('transitionend', () => {
+        btn.classList.remove('active')
+    })
+}
+
+document.addEventListener('keyup', (e) => {
+    const keyUp = e.key
+    if (keyNumbList.includes(keyUp)) {
+        btnNumbs.forEach(num => {
+            if (num.dataset.num === keyUp) {
+                btnActive(num)
+            }
+        })
+        actionNumb(keyUp)
+        return
+    }
+    keyEqual.forEach(item => { checkKeyNotNumbOneItem(item.key, keyUp, actionEqual, btnEqual) })
+    operateKey.forEach(item => {
+        if (item.key === keyUp) {
+            document.querySelectorAll('.btn--operator').forEach(operate => {
+                if (operate.dataset.operator === item.name) {
+                    btnActive(operate)
+                }
+            })
+            actionOperator(item.name)
+            return
+        }
+    })
+    checkKeyNotNumbOneItem(backspace.key, keyUp, actionBackspace, btnBackspace)
+    checkKeyNotNumbOneItem(resetAc.key, keyUp, resetData, btnDelete)
+    checkKeyNotNumbOneItem(pointNumb.key, keyUp, actionPoint, btnPoint)
+})
+function checkKeyNotNumbOneItem(value, key, func, btn) {
+    if (value === key) {
+        func()
+        btnActive(btn)
+        return
+    }
 }
